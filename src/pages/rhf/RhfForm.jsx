213fe68b66815d8cbe00aa89e1b017734d4f7edd
Krishrpatel <krishrpatel250@gmail.com>
2025-11-10 18:31:26 +0530

    edit : Controller component

--- src/pages/rhf/RhfForm.jsx
+++ src/pages/rhf/RhfForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Eye, EyeOff } from "lucide-react"; // icons from lucide-react
+import { Eye, EyeOff } from "lucide-react";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -15,21 +15,21 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const defaultFormValues = {
-  firstName: "thih",
-  lastName: "asdasd",
-  email: "user@gmail.com",
-  mobile: "1234567890",
-  username: "useuser",
-  password: "User@1234",
-  age: "12",
-  isActive: true,
-  userType: "",
-};
-
 import { IZodSchema } from "./IZodSchema";
 import { toast } from "sonner";
 import axios from "axios";
+
+const defaultFormValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  mobile: "",
+  username: "",
+  password: "",
+  age: "",
+  isActive: false,
+  userType: "",
+};
 
 const RhfForm = () => {
   const form = useForm({
@@ -44,40 +44,23 @@ const RhfForm = () => {
 
   const onSubmit = async (data) => {
     try {
-      const res = await axios.post("https://dummyjson.com/users/adds", data, {
+      const res = await axios.post("https://dummyjson.com/users/add", data, {
         headers: { "Content-Type": "application/json" },
       });
-
-      const apiData = {
-        status: true, // false
-        statusCode: 200,
-        message: "Success",
-        data: {
-          name: "Meet",
-        },
-      };
+      
 
       console.log("User added:", res.data);
-      if (apiData.status) {
-        toast.success("Added user successfully");
-        form.reset({ ...defaultFormValues });
-      } else {
-        // handle errors
-        // do something on error
-      }
+      toast.success("Added user successfully");
+      form.reset({ ...defaultFormValues });
     } catch (error) {
       console.error(error);
       toast.error("Something went wrong");
-      // handle errors
-      // do something on error
     }
-
-    // console.log("Form Submitted", data);
   };
 
   return (
     <div className="flex items-center justify-center mt-6">
-      <Card className="w-full max-w-6xl bg-[#cbb3ff] border-2 border-black shadow-lg ">
+      <Card className="w-full max-w-6xl bg-[#cbb3ff] border-2 border-black shadow-lg">
         <CardContent>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-6">
             {/* Firstname */}
@@ -162,20 +145,14 @@ const RhfForm = () => {
               <label htmlFor="password" className="font-medium">
                 Password
               </label>
-
-              {/* Input wrapper */}
               <div className="relative">
                 <Input
                   type={showPassword ? "text" : "password"}
                   id="password"
-                  {...register("password", {
-                    required: "Password is required",
-                  })}
+                  {...register("password")}
                   className="bg-white border-2 border-black pr-10"
                   placeholder="Enter password"
                 />
-
-                {/* Eye toggle button */}
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
@@ -184,15 +161,13 @@ const RhfForm = () => {
                   {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                 </button>
               </div>
-
-              {/* Error message */}
               <p className="text-red-600 text-sm">{errors.password?.message}</p>
             </div>
 
-            {/* Pin */}
+            {/* Age */}
             <div className="flex flex-col gap-2">
               <label htmlFor="age" className="font-medium">
-                Pin
+                Age
               </label>
               <Input
                 type="text"
@@ -248,11 +223,7 @@ const RhfForm = () => {
 
         <CardFooter className="flex gap-2 justify-end mt-4">
           <Button
-            onClick={() => {
-              handleSubmit(onSubmit, (error) => {
-                console.log("error", error);
-              })();
-            }}
+            onClick={handleSubmit(onSubmit)}
             className="rounded-l-full border-2 border-black px-8"
             disabled={!isDirty || !isValid}
           >
@@ -260,9 +231,6 @@ const RhfForm = () => {
           </Button>
         </CardFooter>
       </Card>
-      {/* {process.env.node_env === "developmnent" &&
-      
-    } */}
       <DevTool control={control} />
     </div>
   );
